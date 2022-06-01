4abf4fcb1f04475e8d7e4c65ebcd513159ff02be
Matthieu M <nerggalkore@gmail.com>
2022-06-01 14:02:45 +0200

    swagger gen update

--- API/Startup.cs
+++ API/Startup.cs
@@ -32,7 +32,24 @@ namespace API
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo() { Title = "MyApi", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ToDo API",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Shayne Boyer",
+                        Email = string.Empty,
+                        Url = new Uri("https://twitter.com/spboyer"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under LICX",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
             });
         }
 
@@ -60,6 +77,8 @@ namespace API
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Api v1");
                 //c.RoutePrefix = string.Empty;
             });
+            
+            
         }
     }
 }
