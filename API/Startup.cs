906345e942c611eed38c7ce746b3973a958e7d27
Raphael Perles <progradius@protonmail.com>
2022-06-02 14:09:48 +0200

    test

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,7 +28,8 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //services.AddCors();
+            services.AddCors();
+            /*
             services.AddCors(options =>
             {
                 options.AddPolicy("AllowAnyOrigin", builder =>
@@ -37,6 +38,7 @@ namespace API
                     builder.AllowAnyOrigin();
                 });
             });
+            */
             services.AddDbContext<RecipeContext>(opt => opt.UseInMemoryDatabase("RecipesList"));
             services.AddDbContext<IngredientContext>(opt => opt.UseInMemoryDatabase("IngredientList"));
             services.AddControllers();
@@ -84,7 +86,8 @@ namespace API
                     .AllowAnyHeader();
             });
 */
-            app.UseCors("AllowAnyOrigin");
+            //app.UseCors("AllowAnyOrigin");
+            app.UseCors();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
